f19eb1a45dfdcb16bd34e1b80e2302f9e86efe93
Duncan Robertson <knacknud@gmail.com>
2022-09-07 14:26:07 +0100

    Extension complete

--- src/customer.py
+++ src/customer.py
@@ -1,13 +1,17 @@
 class Customer:
     
-    def __init__(self, name, wallet, age):
+    def __init__(self, name, wallet, age, energy_level):
         self.name = name
         self.wallet = wallet
         self.age = age
+        self.energy_level = energy_level
 
     def buy_a_drink(self, coffee_shop, drink):
+        if self.age > 15 and self.energy_level < 110:
         # reducing the money in its wallet 
-        self.wallet -= drink.price
+            self.wallet -= drink.price
         #  increasing the money in the Coffee Shop's till
-        coffee_shop.till += drink.price
+            coffee_shop.till += drink.price
+        #   increasing energy level by caffeine level
+            self.energy_level += drink.caffeine_level
 
--- tests/customer_test.py
+++ tests/customer_test.py
@@ -6,9 +6,9 @@ from src.drink import Drink
 class TestCustomer(unittest.TestCase):
 
     def setUp(self):
-        self.customer = Customer("John Smith", 50.00, 28)
-        self.drink1 = Drink("Latte", 2.50)
-        self.drink2 = Drink("Mocha", 3.50)
+        self.customer = Customer("John Smith", 50.00, 28, 100)
+        self.drink1 = Drink("Latte", 2.50, 5)
+        self.drink2 = Drink("Mocha", 3.50, 10)
 
         drinks = [self.drink1, self.drink2]
         self.coffee_shop = CoffeeShop("the Prancing Pony", 200.00, drinks)
@@ -18,3 +18,21 @@ class TestCustomer(unittest.TestCase):
         self.customer.buy_a_drink(self.coffee_shop, self.drink1)
         self.assertEqual(self.customer.wallet, 47.50)
         self.assertEqual(self.coffee_shop.till, 202.50)
+
+    def test_age(self):
+
+        self.customer = Customer("Underage Child", 6.70, 13, 200)
+        self.customer.buy_a_drink(self.coffee_shop, self.drink1)
+        self.assertEqual(self.customer.wallet, 6.70)
+        self.assertEqual(self.coffee_shop.till, 200.00)
+
+    def test_energy_increased_by_caffeine_level(self):
+        self.customer.buy_a_drink(self.coffee_shop, self.drink1)
+        self.assertEqual(105, self.customer.energy_level)
+
+    def test_refuse_sale_high_energy(self):
+        self.customer.buy_a_drink(self.coffee_shop, self.drink2)
+        self.customer.buy_a_drink(self.coffee_shop, self.drink2)
+        self.assertEqual(110, self.customer.energy_level)
+
+
